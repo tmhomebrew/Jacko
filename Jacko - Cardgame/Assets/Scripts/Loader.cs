d720f542e8aa05eb8916892df2901ab7be62ba4a
THMATcaa <36071519+ThmaTech@users.noreply.github.com>
2020-03-06 12:49:42 +0100

    Menu

---
+++ Jacko - Cardgame/Assets/Scripts/Loader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    //public static SceneControllerScript Instance;
+
+    public enum Scene
+    {
+        LoadingScreen,
+        TilelScreen,
+        GameScreen
+    }
+
+    private static Action onLoaderCallBack;
+
+    public static void Load(Scene scene)
+    {
+        //Set the loader callback action to load the target scene
+        onLoaderCallBack = () =>
+        {
+            SceneManager.LoadScene(scene.ToString());
+        };
+
+        //Load the loading scene
+        SceneManager.LoadScene(Scene.LoadingScreen.ToString());
+    }
+
+    public static void LoaderCallBack()
+    {
+        //Triggered after the first Update which lets the screen refresh
+        //Execute the loader callback action which will load the target scene
+        if (onLoaderCallBack != null)
+        {
+            onLoaderCallBack();
+            onLoaderCallBack = null;
+        }
+    }
+}
---
+++ Jacko - Cardgame/Assets/Scripts/LoaderCallBack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LoaderCallBack : MonoBehaviour
+{
+    private bool _isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (_isFirstUpdate)
+        {
+            _isFirstUpdate = false;
+            Loader.LoaderCallBack();
+        }
+    }
+}
